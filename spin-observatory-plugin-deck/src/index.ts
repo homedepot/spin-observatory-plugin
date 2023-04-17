22dd739630aa3c0f01509b5cc1b94d4366e981eb
abe garcia <abraham_garcia-ortiz1@homedepot.com>
2023-04-17 15:53:54 -0400

    add overridesComponent function call

--- spin-observatory-plugin-deck/src/index.ts
+++ spin-observatory-plugin-deck/src/index.ts
@@ -1,9 +1,11 @@
 import type { IDeckPlugin } from '@spinnaker/core';
+import { overridesComponent } from '@spinnaker/core';
 import { overrideRegistrationQueue } from '@spinnaker/core';
 import { HeaderOverride } from './components/header/HeaderOverride';
 
 export const plugin: IDeckPlugin = {
   initialize: () => {
     overrideRegistrationQueue.register(HeaderOverride, 'spinnakerHeader');
+    overridesComponent(HeaderOverride, 'spinnakerHeader');
   },
 };
