46b56098efdc6b2354b063a279415845219f1237
abe garcia <abraham_garcia-ortiz1@homedepot.com>
2023-04-18 13:18:13 -0400

    testing new page

---
+++ spin-observatory-plugin-deck/src/components/ExampleView.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+interface IExampleViewProps {}
+
+export function ExampleView(props: IExampleViewProps) {
+  return <h1>Example View</h1>;
+}
--- spin-observatory-plugin-deck/src/index.ts
+++ spin-observatory-plugin-deck/src/index.ts
@@ -1,9 +1,39 @@
-import type { IDeckPlugin } from '@spinnaker/core';
+import type { Application, ApplicationStateProvider, IDeckPlugin } from '@spinnaker/core';
+import { ApplicationDataSourceRegistry, REST } from '@spinnaker/core';
 import { overrideRegistrationQueue } from '@spinnaker/core';
+
+import { ExampleView } from './components/ExampleView';
 import { SpinnakerHeader } from './components/header/SpinnakerHeader';
 
 export const plugin: IDeckPlugin = {
   initialize: () => {
     overrideRegistrationQueue.register(SpinnakerHeader, 'SpinnakerHeader');
+
+    const injector = (window as any).spinnaker.$injector;
+    const applicationState: ApplicationStateProvider = injector.get('applicationState');
+    applicationState.addChildState({
+      name: 'example',
+      url: '/example',
+      views: {
+        insight: {
+          component: ExampleView,
+          $type: 'react',
+        },
+      },
+    });
+
+    ApplicationDataSourceRegistry.registerDataSource({
+      key: 'example',
+      label: 'Example',
+      autoActivate: true,
+      activeState: '**.example.**',
+      visible: true,
+      sref: '.example',
+      defaultData: [],
+      description: 'Example Data Source',
+      iconName: 'artifact',
+      loader: (application: Application) => REST('/example/data').get(),
+      onLoad: (application: Application, data: any) => Promise.resolve(data),
+    });
   },
 };
