02c88888c87ec8e46fde8157437efa5ad8e0ddb3
abe garcia <abraham_garcia-ortiz1@homedepot.com>
2023-04-18 15:48:09 -0400

    testing new page

--- spin-observatory-plugin-deck/src/index.ts
+++ spin-observatory-plugin-deck/src/index.ts
@@ -4,38 +4,36 @@ import { overrideRegistrationQueue } from '@spinnaker/core';
 
 import { ExampleView } from './components/ExampleView';
 import { SpinnakerHeader } from './components/header/SpinnakerHeader';
-import { initState } from './observatory.states';
 
 export const plugin: IDeckPlugin = {
   initialize: () => {
     overrideRegistrationQueue.register(SpinnakerHeader, 'SpinnakerHeader');
-    initState();
 
-    // const injector = (window as any).spinnaker.$injector;
-    // const applicationState: ApplicationStateProvider = injector.get('applicationState');
-    // applicationState.addChildState({
-    //   name: 'example',
-    //   url: '/example',
-    //   views: {
-    //     insight: {
-    //       component: ExampleView,
-    //       $type: 'react',
-    //     },
-    //   },
-    // });
+    const injector = (window as any).spinnaker.$injector;
+    const applicationState: ApplicationStateProvider = injector.get('applicationState');
+    applicationState.addChildState({
+      name: 'observatory',
+      url: '/observatory',
+      views: {
+        insight: {
+          component: ExampleView,
+          $type: 'react',
+        },
+      },
+    });
 
-    // ApplicationDataSourceRegistry.registerDataSource({
-    //   key: 'example',
-    //   label: 'Example',
-    //   autoActivate: true,
-    //   activeState: '**.example.**',
-    //   visible: true,
-    //   sref: '.example',
-    //   defaultData: [],
-    //   description: 'Example Data Source',
-    //   iconName: 'artifact',
-    //   loader: (application: Application) => REST('/example/data').get(),
-    //   onLoad: (application: Application, data: any) => Promise.resolve(data),
-    // });
+    ApplicationDataSourceRegistry.registerDataSource({
+      key: 'observatory',
+      label: 'Observatory',
+      autoActivate: true,
+      activeState: '**.observatory.**',
+      visible: true,
+      sref: '.observatory',
+      defaultData: [],
+      description: 'Example Data Source',
+      iconName: 'artifact',
+      loader: (application: Application) => REST('/applications').path(application.name).path('/pipelineConfigs').get(),
+      onLoad: (application: Application, data: any) => Promise.resolve(data),
+    });
   },
 };
--- spin-observatory-plugin-deck/src/observatory.states.ts
+++ spin-observatory-plugin-deck/src/observatory.states.ts
@@ -1,88 +1,47 @@
-import type { StateParams } from '@uirouter/angularjs';
-import { module } from 'angular';
+// import type { StateParams } from '@uirouter/angularjs';
+// import { module } from 'angular';
 
-import { ProjectHeader } from '@spinnaker/core';
-import type { ApplicationStateProvider } from '@spinnaker/core';
-import { APPLICATION_STATE_PROVIDER } from '@spinnaker/core';
-import type { IProject } from '@spinnaker/core';
-import type { INestedState, StateConfigProvider } from '@spinnaker/core';
-import { STATE_CONFIG_PROVIDER } from '@spinnaker/core';
-import { ProjectReader } from '@spinnaker/core';
-import { ExampleView } from './components/ExampleView';
+// import { ProjectHeader } from '@spinnaker/core';
+// import type { ApplicationStateProvider } from '@spinnaker/core';
+// import { APPLICATION_STATE_PROVIDER } from '@spinnaker/core';
+// import type { IProject } from '@spinnaker/core';
+// import type { INestedState, StateConfigProvider } from '@spinnaker/core';
+// import { STATE_CONFIG_PROVIDER } from '@spinnaker/core';
+// import { ProjectReader } from '@spinnaker/core';
+// import { ExampleView } from './components/ExampleView';
 
-export interface IProjectStateParms extends StateParams {
-  project: string;
-}
+// export interface IProjectStateParms extends StateParams {
+//   project: string;
+// }
 
-export const OBSERVATORY_STATES_CONFIG = 'spin-observatory-plugin.observatory.state.config';
+// export const OBSERVATORY_STATES_CONFIG = 'spin-observatory-plugin.observatory.state.config';
 
-export const initState = () => {
-  module(OBSERVATORY_STATES_CONFIG, [APPLICATION_STATE_PROVIDER, STATE_CONFIG_PROVIDER]).config([
-    'stateConfigProvider',
-    'applicationStateProvider',
-    (stateConfigProvider: StateConfigProvider, applicationStateProvider: ApplicationStateProvider) => {
-      // const project: INestedState = {
-      //   name: 'project',
-      //   url: '/projects/{project}',
-      //   resolve: {
-      //     projectConfiguration: [
-      //       '$stateParams',
-      //       ($stateParams: IProjectStateParms) => {
-      //         return ProjectReader.getProjectConfig($stateParams.project).then(
-      //           (projectConfig: IProject) => projectConfig,
-      //           (): IProject => {
-      //             return {
-      //               id: null,
-      //               name: $stateParams.project,
-      //               email: null,
-      //               config: null,
-      //               notFound: true,
-      //             };
-      //           },
-      //         );
-      //       },
-      //     ],
-      //   },
-      //   views: {
-      //     'main@': {
-      //       component: ProjectHeader,
-      //       $type: 'react',
-      //     },
-      //   },
-      //   data: {
-      //     pageTitleMain: {
-      //       field: 'project',
-      //     },
-      //     history: {
-      //       type: 'projects',
-      //       state: 'home.project',
-      //       keyParams: ['project'],
-      //     },
-      //   },
-      //   children: [],
-      // };
+// export const initState = () => {
+//   module(OBSERVATORY_STATES_CONFIG, [STATE_CONFIG_PROVIDER]).config([
+//     'stateConfigProvider',
+//     (stateConfigProvider: StateConfigProvider) => {
 
-      const observatory: INestedState = {
-        name: 'observatory',
-        url: '/observatory',
-        views: {
-          'main@': {
-            component: ExampleView,
-            $type: 'react',
-          },
-        },
-        data: {
-          pageTitleMain: {
-            label: 'Projects',
-          },
-        },
-      };
+//       const observatory: INestedState = {
+//         name: 'observatory',
+//         url: '/observatory',
+//         views: {
+//           'main@': {
+//             component: ExampleView,
+//             $type: 'react',
+//           },
+//         },
+//         data: {
+//           pageTitleMain: {
+//             label: 'Projects',
+//           },
+//         },
+//       };
 
-      stateConfigProvider.addToRootState(observatory);
-      // stateConfigProvider.addToRootState(project);
-      // applicationStateProvider.addParentState(project, 'detail', '/applications');
+//       stateConfigProvider.addToRootState(observatory);
+//       stateConfigProvider.addToRootState(project);
+//       applicationStateProvider.addParentState(project, 'detail', '/applications');
 
-      // stateConfigProvider.addRewriteRule('/projects/{project}', '/projects/{project}/dashboard');
-    },
-  ]);
-};
+//       stateConfigProvider.addRewriteRule('/projects/{project}', '/projects/{project}/dashboard');
+//     },
+//   ]);
+// };
