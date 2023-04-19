32d2fdd76ce7f7d060115cd95ac7384f84378084
Victor <v.h.homedepot@gmail.com>
2023-04-19 16:22:35 -0400

    observatory

--- spin-observatory-plugin-deck/src/components/PluginContainer.tsx
+++ spin-observatory-plugin-deck/src/components/PluginContainer.tsx
@@ -1,10 +1,16 @@
 import { Application } from '@spinnaker/core';
 import * as React from 'react';
+import { DatePicker } from './DatePicker/DatePicker';
 
 interface IPluginContainerProps {
   app: Application;
 }
 
 export function PluginContainer(props: IPluginContainerProps) {
-  return <div/>;
+  return (
+    <>
+      <h1>Observatory</h1>
+      <DatePicker />
+    </>
+  );
 }
