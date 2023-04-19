d7cc3658c267c6ad45b7bc41d8a4f34dbb8beee8
abe garcia <abraham_garcia-ortiz1@homedepot.com>
2023-04-19 15:17:31 -0400

    testing select

--- spin-observatory-plugin-deck/src/components/PluginContainer.tsx
+++ spin-observatory-plugin-deck/src/components/PluginContainer.tsx
@@ -1,20 +1,24 @@
 import { Application, IPipeline, ReactSelectInput } from '@spinnaker/core';
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface IPluginContainerProps {
   app: Application;
 }
 
 export function PluginContainer({ app }: IPluginContainerProps) {
-  const dataSource = app.getDataSource('observatory');
-  const pipelines = dataSource.data;
+  const [pipelines, setPipelines] = useState<IPipeline[]>([]);
+  const data = app.getDataSource('observatory').data as IPipeline[];
+
+  useEffect(() => {
+    setPipelines(data);
+  }, data);
 
   console.log(pipelines);
   return (
     <div className="flex-container-v">
       <div className="flex-container-h">
         <div className="flex-pull-left">
-          <ReactSelectInput options={pipelines.map((p: IPipeline) => ({ label: p.name, value: p.name }))} />
+          <ReactSelectInput options={pipelines.map((p) => ({ label: p.name, value: p.name }))} />
         </div>
       </div>
     </div>
