39f320fceb662f3993ef709c780c660590355514
abe garcia <abraham_garcia-ortiz1@homedepot.com>
2023-04-19 14:34:58 -0400

    testing select

--- spin-observatory-plugin-deck/src/components/PluginContainer.tsx
+++ spin-observatory-plugin-deck/src/components/PluginContainer.tsx
@@ -5,15 +5,18 @@ interface IPluginContainerProps {
   app: Application;
 }
 
-export function PluginContainer({app}: IPluginContainerProps) {
-  const dataSource = app.getDataSource("observatory")
-  const pipelines = dataSource.data
+export function PluginContainer({ app }: IPluginContainerProps) {
+  const dataSource = app.getDataSource('observatory');
+  const pipelines = dataSource.data;
 
-  return <div className="flex-container-v">
-    <div className="flex-container-h">
-      <div className="flex-pull-left">
-        <ReactSelectInput optionsString={pipelines.map((p: IPipeline) => p.name)}/>
+  console.log(pipelines);
+  return (
+    <div className="flex-container-v">
+      <div className="flex-container-h">
+        <div className="flex-pull-left">
+          <ReactSelectInput optionsString={dataSource.loaded ? pipelines.map((p: IPipeline) => p.name) : []} />
+        </div>
       </div>
     </div>
-  </div>;
+  );
 }
