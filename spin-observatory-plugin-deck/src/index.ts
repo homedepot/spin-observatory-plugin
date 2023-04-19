47f55e895b8555c9f9275723679f6cc3e21df049
Abe Garcia <44239562+abe21412@users.noreply.github.com>
2023-04-19 12:03:01 -0400

    Add observatory option in side panel of applications (#2)
    * Add observatory option in side panel of applications
    
    ---------
    
    Co-authored-by: abe garcia <abraham_garcia-ortiz1@homedepot.com>

--- spin-observatory-plugin-deck/src/index.ts
+++ spin-observatory-plugin-deck/src/index.ts
@@ -1,6 +1,40 @@
-import type { IDeckPlugin } from '@spinnaker/core';
-import { widgetizeStage } from './WidgetizeStage';
+import {
+  Application,
+  ApplicationStateProvider,
+  ApplicationDataSourceRegistry,
+  IDeckPlugin,
+  REST,
+} from '@spinnaker/core';
+
+import { PluginContainer } from './components/PluginContainer';
 
 export const plugin: IDeckPlugin = {
-  stages: [widgetizeStage],
+  initialize: () => {
+    const injector = (window as any).spinnaker.$injector;
+    const applicationState: ApplicationStateProvider = injector.get('applicationState');
+    applicationState.addChildState({
+      name: 'observatory',
+      url: '/observatory',
+      views: {
+        insight: {
+          component: PluginContainer,
+          $type: 'react',
+        },
+      },
+    });
+
+    ApplicationDataSourceRegistry.registerDataSource({
+      key: 'observatory',
+      label: 'Observatory',
+      autoActivate: true,
+      activeState: '**.observatory.**',
+      visible: true,
+      sref: '.observatory',
+      defaultData: [],
+      description: 'Example Data Source',
+      iconName: 'artifact',
+      loader: (application: Application) => REST('/applications').path(application.name).path('pipelineConfigs').get(),
+      onLoad: (application: Application, data: any) => Promise.resolve(data),
+    });
+  },
 };
