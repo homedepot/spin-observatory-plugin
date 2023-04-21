f2108361730fe78db862bd8691e8e77ec33877d3
abe garcia <abraham_garcia-ortiz1@homedepot.com>
2023-04-21 17:01:18 -0400

    fix extra slash in request

--- spin-observatory-plugin-deck/src/services/gateService.ts
+++ spin-observatory-plugin-deck/src/services/gateService.ts
@@ -10,7 +10,7 @@ export const getExecutions = async (appName: string, params: IExecutionsParams) 
   const { pipelineName, pageSize } = params;
   return await REST('/applications')
     .path(appName)
-    .path('/executions/search')
+    .path('executions/search')
     .query({ pipelineName, size: pageSize })
     .get<IExecution[]>();
 };
