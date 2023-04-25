f24f4837fee65e1e648a1de4de588d88c68a48d8
abe garcia <abraham_garcia-ortiz1@homedepot.com>
2023-04-25 11:36:22 -0400

    testing different style for parameter select

--- spin-observatory-plugin-deck/src/components/ParameterSelect.tsx
+++ spin-observatory-plugin-deck/src/components/ParameterSelect.tsx
@@ -1,36 +1,109 @@
-import { IPipeline } from '@spinnaker/core';
-import React from 'react';
-import type { Option } from 'react-select';
-import VirtualizedSelect from 'react-virtualized-select';
+import * as React from 'react';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import ListItemText from '@mui/material/ListItemText';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
 
-import "./parameterselect.less"
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
-interface IParameterSelectProps {
-  pipeline?: IPipeline;
-  selectedParams: string[];
-  setSelectedParams(params: string[]): void;
-}
+const names = [
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder',
+];
 
-export const ParameterSelect = ({ pipeline, selectedParams, setSelectedParams }: IParameterSelectProps) => {
-  const onParameterSelect = (options: Option<string>[]) => {
-    console.log(pipeline);
-    setSelectedParams(options.map((o) => o.value));
+export const ParameterSelect = () => {
+  const [personName, setPersonName] = React.useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
   };
 
   return (
-    <VirtualizedSelect
-      onChange={onParameterSelect}
-      value={selectedParams}
-      disabled={!pipeline}
-      placeholder="Select Parameters..."
-      clearable={true}
-      noResultsText="No parameters"
-      options={!pipeline || !pipeline.parameterConfig ? [] : extractPipelineParams(pipeline)}
-      multi
-    />
+    <div>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
+        <Select
+          labelId="demo-multiple-checkbox-label"
+          id="demo-multiple-checkbox"
+          multiple
+          value={personName}
+          onChange={handleChange}
+          input={<OutlinedInput label="Tag" />}
+          renderValue={(selected) => selected.join(', ')}
+          MenuProps={MenuProps}
+        >
+          {names.map((name) => (
+            <MenuItem key={name} value={name}>
+              <Checkbox checked={personName.indexOf(name) > -1} />
+              <ListItemText primary={name} />
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
   );
-};
+}
 
-const extractPipelineParams = (config: IPipeline): Option<string>[] => {
-  return config.parameterConfig.map((p) => ({ label: p.name, value: p.name }));
-};
+
+// import { IPipeline } from '@spinnaker/core';
+// import React from 'react';
+// import type { Option } from 'react-select';
+// import VirtualizedSelect from 'react-virtualized-select';
+
+// import "./parameterselect.less"
+
+// interface IParameterSelectProps {
+//   pipeline?: IPipeline;
+//   selectedParams: string[];
+//   setSelectedParams(params: string[]): void;
+// }
+
+// export const ParameterSelect = ({ pipeline, selectedParams, setSelectedParams }: IParameterSelectProps) => {
+//   const onParameterSelect = (options: Option<string>[]) => {
+//     console.log(pipeline);
+//     setSelectedParams(options.map((o) => o.value));
+//   };
+
+//   return (
+//     <VirtualizedSelect
+//       onChange={onParameterSelect}
+//       value={selectedParams}
+//       disabled={!pipeline}
+//       placeholder="Select Parameters..."
+//       clearable={true}
+//       noResultsText="No parameters"
+//       options={!pipeline || !pipeline.parameterConfig ? [] : extractPipelineParams(pipeline)}
+//       multi
+//     />
+//   );
+// };
+
+// const extractPipelineParams = (config: IPipeline): Option<string>[] => {
+//   return config.parameterConfig.map((p) => ({ label: p.name, value: p.name }));
+// };
