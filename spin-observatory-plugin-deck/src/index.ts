ae59726ad22258d873edfee5384ced5325ae34dc
abe garcia <abraham_garcia-ortiz1@homedepot.com>
2023-05-02 11:18:44 -0400

    remove console log

--- spin-observatory-plugin-deck/src/index.ts
+++ spin-observatory-plugin-deck/src/index.ts
@@ -12,7 +12,6 @@ import { PluginContainer } from './components/PluginContainer';
 
 export const plugin: IDeckPlugin = {
   initialize: () => {
-    console.log("v0.5.0+28")
     const injector = (window as any).spinnaker.$injector;
     const applicationState: ApplicationStateProvider = injector.get('applicationState');
     applicationState.addChildState({
