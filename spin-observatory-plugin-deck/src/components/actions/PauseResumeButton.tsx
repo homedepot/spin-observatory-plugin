75e9bb0be6f5b7dc23009f9b31bb4d65e8505992
abe garcia <abraham_garcia-ortiz1@homedepot.com>
2023-05-04 13:45:01 -0400

    testing button to pause/resume

--- spin-observatory-plugin-deck/src/components/actions/PauseResumeButton.tsx
+++ spin-observatory-plugin-deck/src/components/actions/PauseResumeButton.tsx
@@ -10,7 +10,7 @@ import Popper from '@material-ui/core/Popper';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import { pauseExecutions, resumeExecutions } from '../../services/gateService';
-import 'actionbuttons.less';
+import './actionbuttons.less';
 
 interface IPauseResumeButtonProps {
   executionIds: string[];
