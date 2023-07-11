9314a08fb974addbe708ff4927a51fcfa4a88a74
Abe Garcia <44239562+abe21412@users.noreply.github.com>
2023-07-11 15:43:19 -0400

    pass parent execution id to broadside (#22)
    * pass parent execution id to broadside
    
    ---------
    
    Co-authored-by: abe garcia <abraham_garcia-ortiz1@homedepot.com>

--- spin-observatory-plugin-deck/src/services/broadside.ts
+++ spin-observatory-plugin-deck/src/services/broadside.ts
@@ -6,24 +6,44 @@ const BROADSIDE_URI = `${SETTINGS.gateUrl}/proxies/broadside/v1/broadsides`;
 // See https://github.com/one-thd/broadside/blob/main/api/swagger.yml#L206
 const retriggerExecutions = ({ executions }: { executions: IExecution[] }) => {
   /***
-   *       application: "clipper"
-   *       pipelineNameOrId: "Generate Clipper X.509 Key Pair"
-   *       amount: 50
-   *       delay: 100
-   *       pipelineBaseParameters: '{ "parameterName": "custom data" }'
+   *       application:             "clipper"
+   *       pipelineNameOrId:        "Generate Clipper X.509 Key Pair"
+   *       amount:                   50
+   *       delay:                    100
+   *       pipelineBaseParameters:  '{ "parameterName": "custom data" }'
    *       pipelineMultiParameters: '[ { "multiParameterName": "custom data" } ]'
+   *       executionID:             'QWERTYUIOP'
    */
 
-  const application = executions[0].application;
-  const pipelineNameOrId = executions[0].name;
-  const amount = executions.length;
-  const pipelineMultiParameters = executions.map((e) => e.trigger.parameters);
+  const broadsideRequest = JSON.stringify({
+    application: executions[0].application,
+    pipelineNameOrId: executions[0].name,
+    amount: executions.length,
+    pipelineMultiParameters: executions.map((e) => e.trigger.parameters),
+    executionID: getParentExecutionId(executions[0]),
+  });
 
   return fetch(BROADSIDE_URI, {
     method: 'POST',
     credentials: 'include',
-    body: JSON.stringify({ application, pipelineNameOrId, amount, pipelineMultiParameters }),
+    body: broadsideRequest,
   });
+};
+
+const getParentExecutionId = (e: IExecution) => {
+  let executionID;
+  const parent = e.trigger.parentExecution;
+  const parameters = e.trigger.parameters;
+
+  if (parent && parent.id) {
+    executionID = parent.id;
+  } else if (parameters && parameters['parentExecutionId']) {
+    executionID = parameters['parentExecutionId'];
+  } else {
+    executionID = e.id;
+  }
+
+  return executionID;
 };
 
 export const broadside = { retriggerExecutions };
